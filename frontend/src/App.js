7053882b923ae622768a300b6c046d8132873146
Praveen <spraveen593@gmail.com>
2022-02-22 18:20:23 +0530

    Fixed: Home url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/Home.jsx";
+import Home from "./Pages/Home/index";
 import Navbar from "./Components/Navbar/index";
 import Signup from "./Pages/Signup/index";
 
