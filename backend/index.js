4ce623d6a63d2d2ce3dababa38dd9cf7dc6f3fb2
Praveen <spraveen593@gmail.com>
2022-02-24 14:22:58 +0530

    Fix: Baseurl and cors

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use(morgan("dev"));
 // Registering Cors
 app.use(
   cors({
+    credentials: true,
     origin: "*",
   })
 );
--- frontend/src/baseurl.js
+++ frontend/src/baseurl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.REACT_APP_MODE !== "dev"
-    ? "https://quizzly-backend.vercel.app/api/v1"
+    ? "https://quizzly-backend.vercel.app/v1/api"
     : "http://localhost:3500/v1/api";
 
 export default baseUrl;
