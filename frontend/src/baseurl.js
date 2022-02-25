5f2bb9b6fe1a180664988039bce36068da474689
Praveen <spraveen593@gmail.com>
2022-02-25 22:36:02 +0530

    Working in Vercel.json

--- frontend/src/baseurl.js
+++ frontend/src/baseurl.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  process.env.REACT_APP_MODE != "dev"
+  process.env.REACT_APP_MODE === "dev"
     ? "http://localhost:3500/v1/api"
     : "https://quizzly-backend.vercel.app/v1/api";
 
