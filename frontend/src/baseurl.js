c1c70d4f0e8999e27bca6f885c7c1933f1ba8db8
Praveen <spraveen593@gmail.com>
2022-02-25 21:11:29 +0530

    Rebased: Vercel.json

--- frontend/src/baseurl.js
+++ frontend/src/baseurl.js
@@ -1,6 +1,6 @@
 const baseUrl =
-  process.env.REACT_APP_MODE == "dev"
-    ? "http://quizzly-backend.vercel.app/v1/api"
+  process.env.REACT_APP_MODE !== "dev"
+    ? "https://quizzly-backend.vercel.app/v1/api"
     : "http://localhost:3500/v1/api";
 
 export default baseUrl;
