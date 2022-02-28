bb5eeb1189bdb5f4162529ad2271594cff84e392
Praveen <spraveen593@gmail.com>
2022-02-28 17:20:15 +0530

    Fixed: React confetti Package

--- frontend/src/Components/Confetti/index.jsx
+++ frontend/src/Components/Confetti/index.jsx
@@ -1,7 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion";
-import Confetti from "https://cdn.skypack.dev/react-confetti@6.0.0";
+import Confetti from "react-confetti";
 import { useState } from "react";
-import animation from "../../helpers/animation";
 const Confettis = () => {
   const [showConfetti, setShowConfetti] = useState(true);
   setTimeout(() => {
@@ -18,7 +17,7 @@ const Confettis = () => {
           <Confetti
             width={window.innerWidth}
             height={window.innerHeight}
-            wind={0.05}
+            wind={0}
           />
         </motion.div>
       )}
