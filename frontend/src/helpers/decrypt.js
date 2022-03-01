8655903fc38200ae16423a520d8dde0ff19fff6d
Praveen <spraveen593@gmail.com>
2022-03-01 20:15:37 +0530

    Feat: Testing Encryption on prod mode

--- frontend/src/helpers/decrypt.js
+++ frontend/src/helpers/decrypt.js
@@ -1,11 +1,10 @@
 import CryptoJS from "crypto-js";
 
 const decrypt = async (data) => {
-  var bytes = CryptoJS.AES.decrypt(
-    data,
-    process.env.REACT_APP_ENCRYPTION_KEY
-  );
+  console.log(process.env.REACT_APP_ENCRYPTION_KEY);
+  var bytes = CryptoJS.AES.decrypt(data, process.env.REACT_APP_ENCRYPTION_KEY);
   var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  console.log(decryptedData);
   return decryptedData;
 };
 
