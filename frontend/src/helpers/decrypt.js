2a60e7a6e1138d841a6703ed311baf0cc4496678
Praveen <spraveen593@gmail.com>
2022-03-02 23:34:51 +0530

    Testing: Encryption & Decryption on production mode

--- frontend/src/helpers/decrypt.js
+++ frontend/src/helpers/decrypt.js
@@ -7,6 +7,8 @@ const decrypt = async (data) => {
       process.env.REACT_APP_ENCRYPTION_KEY
     );
     var originalText = bytes.toString(CryptoJS.enc.Utf8);
+    console.log(process.env.REACT_APP_ENCRYPTION_KEY);
+    console.log(originalText);
     return originalText;
   } else {
     var bytes = CryptoJS.AES.decrypt(
