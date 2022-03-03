face6c6b02bdb90d657841362d9dd3977a09fa60
Praveen <spraveen593@gmail.com>
2022-03-03 17:27:01 +0530

    Testing: Decryption on Vercel

--- backend/services/encryption-services.js
+++ backend/services/encryption-services.js
@@ -6,11 +6,30 @@ dotenv.config();
 class Encryption {
   async encrypt(data) {
     if (typeof data == "string" || typeof data == "number") {
-      var ciphertext = CryptoJS.AES.encrypt(
-        data,
-        process.env.ENCRYPTION_KEY
-      ).toString();
-      return ciphertext;
+      // var ciphertext = CryptoJS.AES.encrypt(
+      //   data,
+      //   process.env.ENCRYPTION_KEY
+      // ).toString();
+      // return ciphertext;
+      let chip = "AES"
+      let skey = process.env.ENCRYPTION_KEY
+      let result = '';
+      try {
+          const key = CryptoJS.enc.Hex.parse(skey);
+          if (chip === 'AES') {
+              const iv = CryptoJS.lib.WordArray.create([0x00, 0x00, 0x00, 0x00]);
+              result = CryptoJS.AES.encrypt(data, key, {
+                  iv: iv,
+                  mode: CryptoJS.mode.CBC,
+              });
+              result = result.ciphertext.toString(CryptoJS.enc.Hex);
+          } else {
+              result = CryptoJS.HmacSHA256(data, key).toString(CryptoJS.enc.Hex);
+          }
+          return result;
+      } catch (error) {
+          throw error;
+      }
     } else {
       var ciphertext = CryptoJS.AES.encrypt(
         JSON.stringify(data),
@@ -24,20 +43,20 @@ class Encryption {
     var originalText = bytes.toString(CryptoJS.enc.Utf8);
     return originalText;
   }
-  async encryptCrypto(data) {
-    var algorithm = "aes-192-cbc"; //algorithm to use
-    const key = crypto.scryptSync(process.env.ENCRYPTION_KEY, "salt", 24); //create key
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
-    const newData = cipher.update(data, "utf8", "hex") + cipher.final("hex");
-    return newData;
+  // async encryptCrypto(data) {
+  //   var algorithm = "aes-192-cbc"; //algorithm to use
+  //   const key = crypto.scryptSync(process.env.ENCRYPTION_KEY, "salt", 24); //create key
+  //   const iv = crypto.randomBytes(16);
+  //   const cipher = crypto.createCipheriv(algorithm, key, iv);
+  //   const newData = cipher.update(data, "utf8", "hex") + cipher.final("hex");
+  //   return newData;
 
-    // Decrypt
-    // const decipher = crypto.createDecipheriv(algorithm, key, iv);
-    // var decrypted =
-    //   decipher.update(encrypted, "hex", "utf8") + decipher.final("utf8"); //deciphered text
-    // console.log(decrypted);
-  }
+  //   // Decrypt
+  //   // const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  //   // var decrypted =
+  //   //   decipher.update(encrypted, "hex", "utf8") + decipher.final("utf8"); //deciphered text
+  //   // console.log(decrypted);
+  // }
 }
 
 export default new Encryption();
--- frontend/src/helpers/decrypt.js
+++ frontend/src/helpers/decrypt.js
@@ -1,24 +1,52 @@
 import CryptoJS from "crypto-js";
 import Utf8 from 'crypto-js/enc-utf8';
 import AES from 'crypto-js/aes';
-const decrypt = async (data) => {
-  if (typeof data === "string" || typeof data === "number") {
-    var bytes = AES.decrypt(
-      data,
-      process.env.REACT_APP_ENCRYPTION_KEY
-    );
-    var originalText = bytes.toString(Utf8);
-    console.log(process.env.REACT_APP_ENCRYPTION_KEY);
-    console.log(originalText);
-    return originalText;
-  } else {
-    var bytes = CryptoJS.AES.decrypt(
-      data,
-      process.env.REACT_APP_ENCRYPTION_KEY
-    );
-    var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-    return decryptedData;
+// const decrypt = async (data) => {
+//   if (typeof data === "string" || typeof data === "number") {
+//     var bytes = AES.decrypt(
+//       data,
+//       process.env.REACT_APP_ENCRYPTION_KEY
+//     );
+//     var originalText = bytes.toString(Utf8);
+//     console.log(process.env.REACT_APP_ENCRYPTION_KEY);
+//     console.log(originalText);
+//     return originalText;
+//   } else {
+//     var bytes = CryptoJS.AES.decrypt(
+//       data,
+//       process.env.REACT_APP_ENCRYPTION_KEY
+//     );
+//     var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+//     return decryptedData;
+//   }
+// };
+
+// export default decrypt;
+
+const DecryptHex = (encryptedStringHex) => {
+  let chip = "AES";
+  let skey = process.env.REACT_APP_ENCRYPTION_KEY
+  let result = '';
+  const key = CryptoJS.enc.Hex.parse(skey);
+  const iv = CryptoJS.lib.WordArray.create([0x00, 0x00, 0x00, 0x00]);
+  try {
+      const array = CryptoJS.enc.Hex.parse(encryptedStringHex);
+      if (chip === 'AES') {
+          result = CryptoJS.AES.decrypt(encryptedStringHex, key, {
+              iv: iv,
+              format: CryptoJS.format.Hex,
+              mode: CryptoJS.mode.CBC,
+          }).toString(CryptoJS.enc.Utf8);
+          console.log('$$$$');
+          console.log('result : ', result);
+          console.log('$$$$');
+      } else {
+          result = array;
+      }
+      return result;
+  } catch (error) {
+      throw error;
   }
 };
 
-export default decrypt;
+export default DecryptHex
