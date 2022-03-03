3b492ddace4bfd473fe2ce8fbf1c09bad2893b99
Praveen <spraveen593@gmail.com>
2022-03-03 16:47:09 +0530

    Testing: Decryption in React/Vercel

--- frontend/src/helpers/decrypt.js
+++ frontend/src/helpers/decrypt.js
@@ -1,13 +1,13 @@
-// import CryptoJS from "crypto-js";
-var CryptoJS = require("crypto-js");
-
+import CryptoJS from "crypto-js";
+import Utf8 from 'crypto-js/enc-utf8';
+import AES from 'crypto-js/aes';
 const decrypt = async (data) => {
   if (typeof data === "string" || typeof data === "number") {
-    var bytes = CryptoJS.AES.decrypt(
+    var bytes = AES.decrypt(
       data,
       process.env.REACT_APP_ENCRYPTION_KEY
     );
-    var originalText = bytes.toString(CryptoJS.enc.Utf8);
+    var originalText = bytes.toString(Utf8);
     console.log(process.env.REACT_APP_ENCRYPTION_KEY);
     console.log(originalText);
     return originalText;
